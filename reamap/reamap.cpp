efd52e939f2f77ab2ed7a37204e708be79fd34ae
Семён Бабушкин <rty.sem@yandex.ru>
2025-11-10 01:08:05 +0300

    init commit

--- reamap/reamap.cpp
+++ reamap/reamap.cpp
@@ -1,20 +1,36 @@
-﻿// reamap.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <list>
+#include <iomanip>
+#include <map>
+#include <string>
 
-#include <iostream>
+template<typename T>
+class Graph {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+private:
+	std::map<T, std::list<T>> adj_list;
+
+public:
+	void add_edge(T x, T y) {
+		adj_list[x].push_back(y);
+		adj_list[y].push_back(x);
+	}
+
+	void print_graph() {
+		for (auto key_value : adj_list) {
+			std::cout << key_value.first << "-> [ ";
+			for (auto elem : key_value.second) {
+				std::cout << elem << " ";
+			}
+			std::cout << "]" << std::endl;
+		}
+	}
+};
+
+int main() {
+	Graph<char> graph;
+	graph.add_edge('A', 'B');
+	graph.add_edge('A', 'C');
+	graph.add_edge('C', 'B');
+	graph.print_graph();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
