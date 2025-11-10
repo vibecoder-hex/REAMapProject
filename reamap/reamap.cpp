c7d8f36e0d4fa98eb15456d55aa390626aed2b05
Семён Бабушкин <rty.sem@yandex.ru>
2025-11-11 02:41:39 +0300

    Добавлен классический BFS алгоритм с хранением родителей каждого узла для поиска кратчайшего пути от узла к узлу

--- reamap/reamap.cpp
+++ reamap/reamap.cpp
@@ -2,7 +2,12 @@
 #include <list>
 #include <iomanip>
 #include <map>
+#include <unordered_map>
 #include <string>
+#include <deque>
+#include <set>
+#include <vector>
+#include <algorithm>
 
 template<typename T>
 class Graph {
@@ -25,12 +30,63 @@ public:
 			std::cout << "]" << std::endl;
 		}
 	}
+
+	std::vector<T> get_shortest_path(T start, T target) {
+
+		if (start == target) {
+			return { start };
+		}
+
+		std::deque<T> deque;
+		std::set<T> visited;
+		std::vector<T> output;
+		std::unordered_map<T, T> parent;
+
+		visited.insert(start);
+		deque.push_back(start);
+
+		while (!deque.empty()) {
+			T current_vertex = deque.front();
+			deque.pop_front();
+			output.push_back(current_vertex);
+
+			if (current_vertex == target) {
+				std::vector<T> shortest_path;
+				T current = target;
+				while (current) {
+					shortest_path.push_back(current);
+					current = parent[current];
+				}
+				std::reverse(shortest_path.begin(), shortest_path.end());
+				return shortest_path;
+			}
+
+			for (T neighbor : adj_list[current_vertex]) {
+				if (!visited.count(neighbor)) {
+					parent[neighbor] = current_vertex;
+					deque.push_back(neighbor);
+					visited.insert(neighbor);
+				}
+			}
+		}
+		return {};
+	}
 };
 
 int main() {
 	Graph<char> graph;
 	graph.add_edge('A', 'B');
-	graph.add_edge('A', 'C');
-	graph.add_edge('C', 'B');
+	graph.add_edge('A', 'E');
+	graph.add_edge('A', 'D');
+	graph.add_edge('B', 'E');
+	graph.add_edge('B', 'C');
+	graph.add_edge('C', 'F');
+	graph.add_edge('F', 'E');
 	graph.print_graph();
+	std::cout << std::endl;
+	std::vector<char> output = graph.get_shortest_path('A', 'C');
+	for (char ch : output) {
+		std::cout << ch << " ";
+	}
+	std::cout << std::endl;
 }
